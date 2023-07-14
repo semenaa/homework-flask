122cec7605c270ac1edb1e6858a1e04d95c91966
Semenaa <semenaa@yandex.ru>
2023-07-14 16:33:16 +0500

    ...

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, jsonify, request
 from flask.views import MethodView
 
-app = Flask('homework-flask')
+app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+
+if __name__ == '__main__':
+    app.run()
