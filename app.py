5efc5b86d8d12f5f1d1558345c41e3a22a4845ee
semenaa <semenaa@yandex.ru>
2023-07-17 05:51:30 +0500

    .....

--- app.py
+++ app.py
@@ -4,9 +4,29 @@ from flask.views import MethodView
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+class UsersView(MethodView):
+
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
+    def patch(self):
+        pass
+
+    def delete(self):
+        pass
+
+
+user_view = UsersView.as_view('users')
+
+
+app.add_url_rule('/users/<int:user_id>', view_func=user_view, methods=['GET', 'PATCH', 'DELETE'])
+app.add_url_rule('/users/', view_func=user_view, methods=['POST'])
+# @app.route('/')
+# def hello():
+#     return 'Hello, World!'
 
 
 if __name__ == '__main__':
--- client.py
+++ client.py
@@ -1,1 +1,21 @@
 import requests
+
+
+
+# response = requests.post(
+#     "http://127.0.0.1:5000/users", json={"name": "user_2", "password": "ff323m3m3232425r2"}
+# )
+# response = requests.patch(
+#     "http://127.0.0.1:5000/users/3", json={"name": "new_name"}
+# )
+response = requests.delete(
+    "http://127.0.0.1:5000/users/1"
+)
+print(response.status_code)
+print(response.json())
+response = requests.get(
+    "http://127.0.0.1:5000/users/1"
+)
+
+print(response.status_code)
+print(response.json())
